d66dcdf006ccc6bd53e3f695f66288583312dcd5
Farg0n <miguelanga1604@gmail.com>
2025-09-26 12:42:28 -0500

    feat: general interface

--- Sistem-Vet-San-Miguel/SisVetSanMiguel/src/Domain/Interfaces/IGeneralCrud.cs
+++ Sistem-Vet-San-Miguel/SisVetSanMiguel/src/Domain/Interfaces/IGeneralCrud.cs
@@ -1,7 +1,9 @@
 namespace SisVetSanMiguel.Domain.Interfaces;
 
+
 public interface IGeneralCrud<T>
 {
+    
     IEnumerable<T> GetAll();   // Listar todos
     T? GetById(int id);        // Buscar por id
     void Add(T entity);        // Crear
