700e5f35e7141a36895e54e98bb60203b9e8d9e5
mateocasta33 <mateocasta33@gmail.com>
2025-09-28 14:53:04 -0500

    se creo el crud de los clientes

--- Sistem-Vet-San-Miguel/SisVetSanMiguel/src/Domain/Models/Client.cs
+++ Sistem-Vet-San-Miguel/SisVetSanMiguel/src/Domain/Models/Client.cs
@@ -3,6 +3,8 @@ namespace SisVetSanMiguel.Domain.Models;
 public class Client : Person
 {
     public int IdPet { get; set; }
+    
+    public Client() {}
     public Client(string name, string document, string email, int idPet) 
         : base(name, document, email)
     {
