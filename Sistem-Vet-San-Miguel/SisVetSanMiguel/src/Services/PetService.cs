918d0686cad6e38eb2f0ebe623dd6d93788e09e7
Farg0n <miguelanga1604@gmail.com>
2025-09-27 23:28:50 -0500

    feat: pet menu metod

--- Sistem-Vet-San-Miguel/SisVetSanMiguel/src/Services/PetService.cs
+++ Sistem-Vet-San-Miguel/SisVetSanMiguel/src/Services/PetService.cs
@@ -2,22 +2,10 @@ namespace SisVetSanMiguel.Services;
 
 //Se "importa" el codigo que se va a usar
 using SisVetSanMiguel.Domain.Interfaces;
-using SisVetSanMiguel.Domain.Models;
 using System.Collections.Generic;
 using System.Linq;
 
-public class PetService : IGeneralCrud<Pet>
 {
-
-    //Esto hace ......
-    private readonly AppDbContext _context;
-    
-    //Este constructor es para .....
-    public PetService(AppDbContext context)
-    {
-        _context = context;
-    }
-    
     //Metodo del menu
     //--------------------------------------------------------------------------------------------------
     public static void PetMenu()
@@ -25,7 +13,6 @@ public class PetService : IGeneralCrud<Pet>
         //variable para poder salir de este sub menu
         bool state = true;
         
-        while (state)
         {
             VisualMenu();
 
@@ -51,48 +38,4 @@ public class PetService : IGeneralCrud<Pet>
     }
     //--------------------------------------------------------------------------------------------------
     
-    //Metodo de bienvenida de menu
-    //--------------------------------------------------------------------------------------------------
-    private static void VisualMenu()
-    {
-        //Mensage de bienvenida
-        Console.WriteLine("----------------------------------------");
-        Console.WriteLine("==== Bienvenido a el menu de mascota ===");
-        Console.WriteLine("----------------------------------------");
-        Console.WriteLine("=== Digite la opcion que desea ===");
-        Console.WriteLine("- 1 Registrar mascota\n- 2 Ver mascotas registradas\n- 3 Editar mascota\n- 4 Eliminar mascota\n- 5 Volver al menu principal");
-    }
-    //--------------------------------------------------------------------------------------------------
-    
-    //Metodo de registro de mascota
-    //--------------------------------------------------------------------------------------------------
-    private static void RegisterPet()
-    {
-        
-    } 
-    //--------------------------------------------------------------------------------------------------
-    
-    //Metodo de mostrar lista de mascotas
-    //--------------------------------------------------------------------------------------------------
-    public IEnumerable<Pet> GetAll()
-    {
-        return _context.pets.ToList();
-    }
-    //--------------------------------------------------------------------------------------------------
-    
-    //Metodo para editar mascota
-    //--------------------------------------------------------------------------------------------------
-    private static void EditPet()
-    {
-        
-    }
-    //--------------------------------------------------------------------------------------------------
-    
-    //Metodo para eliminar mascota
-    //--------------------------------------------------------------------------------------------------
-    private static void DeletePet()
-    {
-        
-    }
-    //--------------------------------------------------------------------------------------------------
 }
