8e7650e06309693362d7a5a4b52efaf8cabb1676
Farg0n <miguelanga1604@gmail.com>
2025-09-28 01:39:36 -0500

    feat: new coment

--- Sistem-Vet-San-Miguel/SisVetSanMiguel/src/Domain/Interfaces/IGeneralCrud.cs
+++ Sistem-Vet-San-Miguel/SisVetSanMiguel/src/Domain/Interfaces/IGeneralCrud.cs
@@ -4,6 +4,7 @@ namespace SisVetSanMiguel.Domain.Interfaces;
 public interface IGeneralCrud<T>
 {
     
+    //Estructura para una interfaz general para todos los servicios
     IEnumerable<T> GetAll();   // Listar todos
     T? GetById(int id);        // Buscar por id
     void Add(T entity);        // Crear
