11e29adbd21891e1a885e7b3fdca60079ccfe694
Farg0n <miguelanga1604@gmail.com>
2025-10-08 17:15:05 -0500

    Initial commit

---
+++ Video-Games-Catalog-Folder/VideoGameCatalog/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace VideoGameCatalog.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
