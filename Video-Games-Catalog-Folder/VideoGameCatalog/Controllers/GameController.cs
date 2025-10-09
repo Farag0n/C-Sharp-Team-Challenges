bf16f7a3e232d08918b153e31ed098a56ba69d3f
Farg0n <miguelanga1604@gmail.com>
2025-10-08 19:03:14 -0500

    feat: agregar juego v2

--- Video-Games-Catalog-Folder/VideoGameCatalog/Controllers/GameController.cs
+++ Video-Games-Catalog-Folder/VideoGameCatalog/Controllers/GameController.cs
@@ -26,39 +26,93 @@ public class GameController : Controller
     }
     
 
-    // Añadir juego
+    //Agregar juegos version 2
     //---------------------------------------------------------------------------------------
     [HttpPost]
     public async Task<IActionResult> AddGames(string nameGame)
     {
         string apiKey = _config["ApiKey"]!;
         string apiUrl = $"https://api.rawg.io/api/games?search={nameGame}&key={apiKey}";
+
         try
         {
-            HttpClient client = new HttpClient();
+            using HttpClient client = new HttpClient();
             var result = await client.GetStringAsync(apiUrl);
+            using JsonDocument doc = JsonDocument.Parse(result);
 
-            JsonDocument doc = JsonDocument.Parse(result);
-            var name = doc.RootElement.GetProperty("name").GetString();
-            var genres = doc.RootElement.GetProperty("genres").GetString();
-            var image = doc.RootElement.GetProperty("background_image").GetString();
+            // Acceder al primer resultado del array "results"
+            var results = doc.RootElement.GetProperty("results");
+            if (results.GetArrayLength() == 0)
+            {
+                Console.WriteLine("No se encontraron resultados para el juego buscado.");
+                return RedirectToAction("Index");
+            }
 
-            var game = new Game(name, null, 0, image);
+            var firstGame = results[0];
+            var name = firstGame.GetProperty("name").GetString();
+            var image = firstGame.GetProperty("background_image").GetString();
+
+            // Si el juego tiene géneros, los concatenamos
+            string genres = "";
+            if (firstGame.TryGetProperty("genres", out var genresArray))
+            {
+                genres = string.Join(", ", genresArray.EnumerateArray().Select(g => g.GetProperty("name").GetString()));
+            }
+
+            var game = new Game(name, genres, 0, image);
             _context.Games.Add(game);
+            await _context.SaveChangesAsync(); // ¡no olvide guardar!
 
             return RedirectToAction("Index");
         }
         catch (HttpRequestException e)
         {
-            Console.WriteLine(e.InnerException.Message);
+            Console.WriteLine($"Error al conectar con la API: {e.Message}");
         }
         catch (Exception e)
         {
-            Console.WriteLine(e);
+            Console.WriteLine($"Error inesperado: {e.Message}");
             throw;
         }
+
         return RedirectToAction("Index");
     }
+
+    
+    
+    // Añadir juego
+    //---------------------------------------------------------------------------------------
+    // [HttpPost]
+    // public async Task<IActionResult> AddGames(string nameGame)
+    // {
+    //     string apiKey = _config["ApiKey"]!;
+    //     string apiUrl = $"https://api.rawg.io/api/games?search={nameGame}&key={apiKey}";
+    //     try
+    //     {
+    //         HttpClient client = new HttpClient();
+    //         var result = await client.GetStringAsync(apiUrl);
+    //
+    //         JsonDocument doc = JsonDocument.Parse(result);
+    //         var name = doc.RootElement.GetProperty("name").GetString();
+    //         var genres = doc.RootElement.GetProperty("genres").GetString();
+    //         var image = doc.RootElement.GetProperty("background_image").GetString();
+    //
+    //         var game = new Game(name, null, 0, image);
+    //         _context.Games.Add(game);
+    //
+    //         return RedirectToAction("Index");
+    //     }
+    //     catch (HttpRequestException e)
+    //     {
+    //         Console.WriteLine(e.InnerException.Message);
+    //     }
+    //     catch (Exception e)
+    //     {
+    //         Console.WriteLine(e);
+    //         throw;
+    //     }
+    //     return RedirectToAction("Index");
+    // }
     
     //Ver mas
     //---------------------------------------------------------------------------------------
